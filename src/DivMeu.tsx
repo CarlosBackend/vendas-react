3e88f963934fb85254b478995197410dc16f0c0a
Carlos Henrique <carlinhosbackend@gmail.com>
2025-07-30 20:43:56 -0300

    feat: create module login

--- src/DivMeu.tsx
+++
@@ -1,9 +0,0 @@
-const DivMeu = ({ Titulo }) => {
-  return (
-    <div>
-      <h1>{Titulo}</h1>
-      <h2>Subtitulo</h2>
-    </div>
-  );
-};
-export default DivMeu;
---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackGroundImage } from '../../styles/loginScreen.style';
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <BackGroundImage src="./logo.png" />
+    </div>
+  );
+};
+
+export default LoginScreen;
