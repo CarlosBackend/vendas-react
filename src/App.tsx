c683514c25cc69cab75b60f3daee0e15f9b14ab3
Carlos Henrique <carlinhosbackend@gmail.com>
2025-07-30 20:13:00 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import DivMeu from './DivMeu';
+function App() {
+  return (
+    <div>
+      <DivMeu Titulo="Carlos" />
+      <DivMeu Titulo="Maria" />
+      <DivMeu Titulo="Ana" />
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur ad,
+          aperiam nemo hic architecto tempora sunt doloremque nam, libero minima
+          officiis aliquid consequuntur, minus nostrum nobis deleniti cum?
+          Dolor, porro.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
